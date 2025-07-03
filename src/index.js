a04d98e0f7aa8e02cc1bd8202a87627a5c100754
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-07-03 11:19:03 +0530

    app-express-setup, utils-error-response-async-setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import {connectDB} from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({path: "./.env"});
-connectDB();
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+.then(() => {
+  app.on('error', (err) => {
+    console.error("Server error:", err);
+  });
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
+.catch((err) => {
+  console.error("Database connection failed:", err);
+  process.exit(1);
+});
